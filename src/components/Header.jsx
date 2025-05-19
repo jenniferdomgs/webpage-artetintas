d5ce3302d3fd2337c4dc587bc5872b5395eabfa7
Jennifer <99359081+jenniferdomgs@users.noreply.github.com>
2025-05-19 19:14:16 -0300

    header e hero - ok

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import logo from '../img/logo.png'
+import { Menu, X } from 'lucide-react'
+import '../App.css'
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <header>
+      <div className="header-container">
+        <div className="logo-nav">
+            <img src={logo} alt="Logo" className="logo" />
+            <nav className="nav-desktop">
+                <a href="#sobre">Quem sou eu?</a>
+                <a href="#servicos">Serviços</a>
+                <a href="#trabalhos">Trabalhos</a>
+                <a href="#contato">Contato</a>
+            </nav>
+        </div>
+
+
+        <a href="#whatsApp" className="whatsapp-btn">
+          <i className="fa-brands fa-whatsapp"></i> WhatsApp
+        </a>
+
+        <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+      </div>
+
+      {menuOpen && (
+        <div className="menu-mobile">
+          <a href="#sobre">Quem sou eu?</a>
+          <a href="#servicos">Serviços</a>
+          <a href="#trabalhos">Trabalhos</a>
+          <a href="#contato">Contato</a>
+          <a href="#whatsApp" className="whatsapp-btn">
+            <i className="fa-brands fa-whatsapp"></i> WhatsApp
+          </a>
+        </div>
+      )}
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Adailson Martins</h1>
+        <p>Serviços & Pinturas</p>
+        <a href="#contato" className="hero-button">Venha nos conhecer</a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
